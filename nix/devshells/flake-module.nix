79f1961bb4edd69a568a8df9008df310f0bf0a55
Wael Nasreddine <wael.nasreddine@gmail.com>
2024-12-20 21:25:11 -0800

    remove dbmate

--- nix/devshells/flake-module.nix
+++ nix/devshells/flake-module.nix
@@ -4,16 +4,13 @@
     {
       devShells.default = pkgs.mkShell {
         buildInputs = [
-          pkgs.dbmate
           pkgs.delve
           pkgs.go
           pkgs.golangci-lint
-          pkgs.sqlc
           pkgs.watchexec
         ];
 
         _GO_VERSION = "${pkgs.go.version}";
-        _DBMATE_VERSION = "${pkgs.dbmate.version}";
 
         # Disable hardening for fortify otherwize it's not possible to use Delve.
         hardeningDisable = [ "fortify" ];
